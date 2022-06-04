468064db65957feb2f06127e0d96dfba2741276d
BiagioFesta <15035284+BiagioFesta@users.noreply.github.com>
2022-06-04 17:01:29 +0200

    polybar: script waits for termination

--- polybar/polybar-restart.sh
+++ polybar/polybar-restart.sh
@@ -7,5 +7,8 @@ POLYBAR_MSG=$(which polybar-msg)
 
 set +e
 
+# Quit polybar and wait termination
 ${POLYBAR_MSG} cmd quit
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
 ${POLYBAR} 2>&1 | tee /tmp/polybar.log & disown
