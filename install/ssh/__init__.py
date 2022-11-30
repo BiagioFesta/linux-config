22d750dc6496faad1e8d802af82ada632d2881a1
BiagioFesta <15035284+BiagioFesta@users.noreply.github.com>
2022-11-30 20:22:38 +0100

    ssh: check configuration for adding key to agent

--- install/ssh/__init__.py
+++ install/ssh/__init__.py
@@ -4,6 +4,7 @@ import os
 
 
 SYSTEMD_SERVICE_UNIT_DIR = '.config/systemd/user'
+SSH_CONFIG = '.ssh/config'
 
 
 def install():
@@ -16,3 +17,8 @@ def check():
     utilities.fs.check_path(SYSTEMD_SERVICE_UNIT_DIR)
     utilities.system.check_binary('ssh-agent')
     utilities.system.check_systemd_unit('ssh-agent')
+
+    utilities.fs.check_file(SSH_CONFIG)
+    if utilities.fs.is_file_exists(SSH_CONFIG):
+        if not utilities.fs.lookup_line(SSH_CONFIG, 'AddKeysToAgent yes'):
+            print("[WARN]: 'AddKeysToAgent yes' is missing in ssh config")
