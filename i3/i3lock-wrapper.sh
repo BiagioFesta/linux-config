4a1bf35b17cd531194fcba7f233000e424c79bc8
BiagioFesta <15035284+BiagioFesta@users.noreply.github.com>
2022-12-08 14:47:37 +0100

    i3/lockwrapper: set ffmpeg resolution on screen capture

--- i3/i3lock-wrapper.sh
+++ i3/i3lock-wrapper.sh
@@ -5,7 +5,8 @@ IS_DUNST_PAUSED="$(dunstctl is-paused)"
 
 dunstctl set-paused true
 
-ffmpeg -f x11grab -y -i "${DISPLAY}" -filter_complex "boxblur=5:1" -vframes 1 "${IMAGE}" -loglevel quiet
+RES=$(xrandr | grep 'current' | sed -E 's/.*current\s([0-9]+)\sx\s([0-9]+).*/\1x\2/')
+ffmpeg -f x11grab -video_size "${RES}" -y -i "${DISPLAY}" -filter_complex "boxblur=5:1" -vframes 1 "${IMAGE}" -loglevel quiet
 
 i3lock -f -i "${IMAGE}" --nofork
 
